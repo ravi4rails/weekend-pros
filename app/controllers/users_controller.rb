d66676ee68bb5433abbf08059d01f80f210041b9
Shashi Singh <shashi.we@gmail.com>
2018-05-22 01:03:08 +0530

    worked on user show and index pahge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
+  
   def index
+    if params[:skill].present?
+      @users = User.all.map {|u| u if u.skill_list.include? params[:skill]}.compact
+    else 
+      @users = User.all
+    end
   end
 
   def show
+    @user = User.find(params[:id])
   end
 end
