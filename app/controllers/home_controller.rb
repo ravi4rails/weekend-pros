2851e6e5125b030079a7bfdb3ec590ec0f40f541
ravi4rails <ravionrails@gmail.com>
2018-05-29 16:47:06 +0530

    create custom layout for sign in page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,8 +10,8 @@ class HomeController < ApplicationController
   end
 
   def pro_listing
-    if params[:skill].present?
-      if params[:skill] == "Teaching"
+    if params[:skill].present? 
+      if params[:skill] == "Teaching" 
         @users = User.where(primary_skill: params[:skill]).page params[:page]
       elsif params[:skill] == "Web Development"
         @users = User.where(primary_skill: params[:skill]).page params[:page]
