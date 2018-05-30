9deac82b14ebf6f93f32122dcdbf2561547c61d2
ravi4rails <ravionrails@gmail.com>
2018-05-30 15:26:17 +0530

    quik fix

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -69,6 +69,6 @@ class SkillsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def skill_params
-      params.require(:skill).permit(:name, :list_view)
+      params.require(:skill).permit(:name)
     end
 end
