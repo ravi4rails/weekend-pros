e554241a3d0485571e107ba32562a78be58db14f
ravi4rails <ravionrails@gmail.com>
2018-07-10 18:52:15 +0530

    add letures

---
+++ db/migrate/20180710092848_create_lectures.rb
@@ -0,0 +1,10 @@
+class CreateLectures < ActiveRecord::Migration[5.1]
+  def change
+    create_table :lectures do |t|
+      t.string :topic_name
+      t.references :course, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
